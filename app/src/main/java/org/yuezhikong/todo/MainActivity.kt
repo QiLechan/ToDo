d769fd3a29fe98d5d168d6de50fb38715b3d1f72
QiLechan <qilechan@outlook.com>
2026-04-10 16:38:52 +0800

    Initial commit

---
+++ app/src/main/java/org/yuezhikong/todo/MainActivity.kt
@@ -0,0 +1,111 @@
+package org.yuezhikong.todo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.ui.NavDisplay
+import org.yuezhikong.todo.ui.home.CalendarScreen
+import org.yuezhikong.todo.ui.home.HomeScreen
+import org.yuezhikong.todo.ui.home.UserScreen
+import org.yuezhikong.todo.ui.theme.ToDoTheme
+
+data object Home
+data object Calendar
+data object User
+data class Product(val id: String)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ToDoTheme {
+                ToDo()
+            }
+        }
+    }
+}
+
+@Composable
+fun ToDo() {
+    val backStack = remember { mutableStateListOf<Any>(Home) }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        bottomBar = { BottomBar(backStack.last().toString(), backStack) }
+    ) { paddingValues ->
+        NavDisplay(
+            backStack = backStack,
+            onBack = { backStack.removeLastOrNull() },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            entryProvider = { key ->
+                when (key) {
+                    is Home -> NavEntry(key) {
+                        HomeScreen()
+                    }
+                    is Calendar -> NavEntry(key) {
+                        CalendarScreen()
+                    }
+                    is User -> NavEntry(key) {
+                        UserScreen()
+                    }
+                    else -> NavEntry(Unit) { Text("Unknown route") }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun BottomBar(currentRoute: String, backStack: SnapshotStateList<Any>) {
+    NavigationBar{
+        NavigationBarItem(
+            selected = currentRoute == "Home",
+            onClick = {
+                backStack.removeLastOrNull()
+                backStack.add(Home)
+            },
+            icon = { Icon(Icons.Filled.Home, null) },
+            label = { Text("首页") }
+        )
+        NavigationBarItem(
+            selected = currentRoute == "Calendar",
+            onClick = {
+                backStack.removeLastOrNull()
+                backStack.add(Calendar)
+            },
+            icon = { Icon(Icons.Filled.DateRange, null) },
+            label = { Text("日历") }
+        )
+        NavigationBarItem(
+            selected = currentRoute == "User",
+            onClick = {
+                backStack.removeLastOrNull()
+                backStack.add(User)
+            },
+            icon = { Icon(Icons.Filled.Person, null) },
+            label = { Text("我的") }
+        )
+    }
+}
---
+++ app/src/main/java/org/yuezhikong/todo/ui/home/HomeScreen.kt
@@ -0,0 +1,28 @@
+package org.yuezhikong.todo.ui.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) {paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) { Text("测试") }
+    }
+}
