31bba4eea72d383e0ebf65caf44ffb0a0b5097fd
QiLechan <qilechan@outlook.com>
2026-04-17 00:07:40 +0800

    feat: 添加提醒选项，添加提醒时间选择界面

--- app/src/main/java/org/yuezhikong/todo/ui/add/AddScreen.kt
+++ app/src/main/java/org/yuezhikong/todo/ui/add/AddScreen.kt
@@ -1,21 +1,18 @@
 package org.yuezhikong.todo.ui.add
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -23,9 +20,11 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,6 +40,7 @@ fun AddScreen() {
     var name by remember { mutableStateOf("") }
     var allDay by remember { mutableStateOf(false) }
     var alarm by remember { mutableStateOf(false) }
+    var noticeTimes = remember { mutableStateListOf(1) }
 
     Scaffold(
         topBar = {
@@ -61,7 +61,9 @@ fun AddScreen() {
                     onValueChange = { name = it },
                     label = { Text("标题") },
                     singleLine = true,
-                    modifier = Modifier.fillMaxWidth().height(56.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
                     shape = RoundedCornerShape(16.dp),
                     colors = TextFieldDefaults.colors(
                         focusedIndicatorColor = Color.Transparent,
@@ -87,11 +89,27 @@ fun AddScreen() {
                     shape = RoundedCornerShape(16.dp)
                 ){
                     ChooseWidget("提醒") {
-                        NoticeSettings()
+                        NoticeSettings(
+                            noticetime = {
+                                if (it == 0){
+                                    noticeTimes.clear()
+                                    noticeTimes.add(0)
+                                }
+                                else {
+                                    if (it in noticeTimes) {
+                                        noticeTimes.remove(it)
+                                    } else
+                                        noticeTimes.remove(0)
+                                        noticeTimes.add(it)
+                                }
+                                         },
+                            current = noticeTimes)
                     }
-                    SwitchWidget(title = "闹钟提醒", checked = alarm, onCheckedChange = { alarm = it })
-                    if (alarm) {
-                        DatePickerWidget("提醒时间")
+                    if (0 !in noticeTimes) {
+                        SwitchWidget(
+                            title = "闹钟提醒",
+                            checked = alarm,
+                            onCheckedChange = { alarm = it })
                     }
                 }
             }
@@ -99,28 +117,44 @@ fun AddScreen() {
     }
 }
 
-@Preview
 @Composable
-fun NoticeSettings() {
+fun NoticeSettings(current: SnapshotStateList<Int>, noticetime: (Int) -> Unit = {} ) {
     Column {
-        ListItem(
-            headlineContent = { Text("不提醒") },
-        )
-        HorizontalDivider()
-        ListItem(
-            headlineContent = { Text("提前5分钟") },
-        )
-        HorizontalDivider()
-        ListItem(
-            headlineContent = { Text("提前15分钟") },
-        )
-        HorizontalDivider()
-        ListItem(
-            headlineContent = { Text("提前30分钟") },
-        )
-        HorizontalDivider()
-        ListItem(
-            headlineContent = { Text("提前1小时") },
-        )
+        OptionItem("无提醒", selected = 0 in current) {
+            noticetime(0)
+        }
+        OptionItem("日程开始时", selected = 1 in current) {
+            noticetime(1)
+        }
+        OptionItem("日程前5分钟", selected = 2 in current) {
+            noticetime(2)
+        }
+        OptionItem("日程前15分钟", selected = 3 in current) {
+            noticetime(3)
+        }
+        OptionItem("日程前30分钟", selected = 4 in current) {
+            noticetime(4)
+        }
+        OptionItem("日程前1小时", selected = 5 in current) {
+            noticetime(5)
+        }
     }
+}
+
+@Composable
+fun OptionItem(
+    text: String,
+    selected: Boolean = false,
+    onClick: () -> Unit
+) {
+    ListItem(
+        headlineContent = { Text(text) },
+        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+        modifier = Modifier.clickable(
+            onClick = { onClick() }
+        ),
+        trailingContent = {
+            if (selected) { Icon(Icons.Default.Check, contentDescription = "Selected") }
+        },
+    )
 }
